9a181126da8a9dc19019d7bbf8b34d09ccdac3b0
ibrahim olayiwola <alaayaibrahim@gmail.com>
2024-10-06 23:09:13 +0100

    new reactnative

---
+++ app/Homescreen.js
@@ -0,0 +1,17 @@
+import { Text, View,TextInput,StyleSheet,Button,Image,
+    SafeAreaView,StatusBar,
+    FlatList,ScrollView,ActivityIndicator
+  
+  
+  } from "react-native";
+
+
+function Homescreen() {
+  return (
+    <div>
+      <Text>hello asshole</Text>
+    </div>
+  );
+}
+
+export default Homescreen;
